a889f800e0250420800b5f5e748c00fdfab68c4d
Omerg970 <omer9y@proton.me>
2026-04-28 21:03:10 +0300

    Proje yapısı ve README eklendi

---
+++ README.md
@@ -0,0 +1,8 @@
+Bu projede hem algoritmaları anlayıp koda dökmeyi hem de githuba küçük bir projeyi nasıl uygun bir şekilde konsol üzerinden yüklemeyi öğreniyorum.
+
+##algoritma hakkında
+dizideki en küçük elemanı hızlıca bulup onu dizinin başına yerleştirme özelliğiyle tanınıyor.
+
+Kod blokların yanlarında özgünlüğüm ile yorum satırlarımı eklemiş bulunmaktayım.
+
+Sevgilerle Muhammed Ömer Gün
---
+++ src/proje 15(selection sort algoritması).cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+using namespace std;
+
+void siralama(int* dizi, int adet)
+{
+	for (int i = 0; i < adet; i++)
+	{
+		for (int j = i + 1; j < adet; j++)
+		{
+			if(dizi[i] > dizi[j]) // iki döngüye ihtiyaç duydum.. ilk döngü referansı alır, ikinci döngü referans ile karşılaştırılacak elemanı.
+			{
+				int gecici = dizi[i]; //yer değiştirmeler burada yapılıyor.
+				dizi[i] = dizi[j];
+				dizi[j] = gecici;
+			}
+			//else
+			//{
+			//	continue;
+			//} // bu kısmı sildim çünkü gereksiz olduğunu düşündüm.. else olmasa bile zaten döngü diğer adıma geçecektir continue olmasa bile..
+		}
+	}
+	for (int a = 0; a < adet; a++)
+	{
+		cout << dizi[a] << " ";
+	}
+
+}
+
+
+
+
+
+
+
+
+int main()
+{
+
+	int dizi[] = { 2, 6, 3, 4, 9, 23, 1 }; //şimdilik sıradan bir dizi..
+	siralama(dizi, 7);
+
+}
+
